11cac530ea9500addc46fc9151d4f0704324c64b
sl846 <shiyu.liu2@duke.edu>
2025-03-08 23:17:49 -0500

    created initial MineBuilding.java class without policy and request, waiting to be tested

---
+++ app/src/main/java/edu/duke/ece651/factorysim/MineBuilding.java
@@ -0,0 +1,46 @@
+package edu.duke.ece651.factorysim;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a mine building in the simulation.
+ */
+public class MineBuilding extends Building {
+  private final Recipe miningRecipe;
+  private final Item resource; // got from miningRecipe
+  private final int miningLatency; // got from miningRecipe
+
+  /**
+   * Constructs a basic mine with empty storage and no ingredient source
+   * buildings.
+   * 
+   * @param miningRecipe is the recipe for this mine.
+   * @param name         is the name of the building.
+   * @throws IllegalArgumentException if the name is not valid.
+   */
+  public MineBuilding(Recipe miningRecipe, String name) {
+    super(name, new ArrayList<Building>());
+    this.miningRecipe = miningRecipe;
+    this.resource = miningRecipe.getOutput();
+    this.miningLatency = miningRecipe.getLatency();
+  }
+
+  /**
+   * Gets the resource of this mine.
+   * 
+   * @return the resource item of this mine.
+   */
+  public Item getResource() {
+    return resource;
+  }
+
+  /**
+   * Gets the mining latency of this mine (i.e. how many time steps to consume to
+   * mine 1 resource item)
+   * 
+   * @return the mining latency of this mine.
+   */
+  public int getMiningLatency() {
+    return miningLatency;
+  }
+}
