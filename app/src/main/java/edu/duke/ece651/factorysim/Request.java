915252223ea74d34f7c620e14d4042cc7f4b2288
sl846 <shiyu.liu2@duke.edu>
2025-03-13 18:16:04 -0400

    refactored Request.java class

--- app/src/main/java/edu/duke/ece651/factorysim/Request.java
+++ app/src/main/java/edu/duke/ece651/factorysim/Request.java
@@ -1,50 +1,86 @@
 package edu.duke.ece651.factorysim;
 
 /**
- * Represents a request for an item from a building.
+ * Represents a request for an item in the simulation.
+ * The request can either come from user to a building, or from a building to
+ * another building.
  */
 public class Request {
-    private String item;
-    private Building requester;
-    private boolean isUserRequest;
+  private final int orderNum;
+  private final Item item;
+  private final Recipe recipe;
+  private final Building producer;
+  private final Building deliverTo;
+  private boolean isUserRequest;
 
-    /**
-     * Creates a new request.
-     *
-     * @param item the requested item.
-     * @param requester the building making the request.
-     * @param isUserRequest true if the request was made by a user from system.in.
-     */
-    public Request(String item, Building requester,boolean isUserRequest) {
-        this.item = item;
-        this.requester = requester;
-        this.isUserRequest = isUserRequest;
-    }
+  /**
+   * Constructs a request.
+   * 
+   * @param orderNum      is the order number.
+   * @param item          is the item being requested.
+   * @param recipe        is the recipe for the requested item.
+   * @param producer      is the producer building for the requested item (and
+   *                      recipe).
+   * @param deliverTo     is the target building to receive the requested item.
+   * @param isUserRequest is the boolean to tell if this request is made by user.
+   */
+  public Request(int orderNum, Item item, Recipe recipe, Building producer, Building deliverTo, boolean isUserRequest) {
+    this.orderNum = orderNum;
+    this.item = item;
+    this.recipe = recipe;
+    this.producer = producer;
+    this.deliverTo = deliverTo;
+    this.isUserRequest = isUserRequest;
+  }
 
-    /**
-     * Gets the requested item name.
-     *
-     * @return the item name.
-     */
-    public String getItem() {
-        return item;
-    }
+  /**
+   * Gets the order number of this request.
+   *@return the order number of this request.
+   */
+  public int getOrderNum() {
+    return orderNum;
+  }
 
-    /**
-     * Gets the building making the request.
-     *
-     * @return the building.
-     */
-    public Building getRequester() {
-        return requester;
-    }
+  /**
+   * Gets the requested item.
+   *@return the requested item.
+   */
+  public Item getItem() {
+    return item;
+  }
+  /**
+   * Gets the recipe for the requested item.
+   *@return the recipe for the requested item.
+   */
+  public Recipe getRecipe() {
+    return recipe;
+  }
+  /**
+   * Gets the producer building to produce the requested item.
+   *@return the producer building.
+   */
+  public Building getProducer() {
+    return producer;
+  }
 
-    /**
-     * Checks if the request was made by a user.
-     *
-     * @return true if the request was made by a user from system.in
-     */
-    public boolean isUserRequest() {
-        return isUserRequest;
+  /**
+   * Gets the target delivery building for the requested item.
+   * 
+   * @return the target delivery building if the request is issued from another
+   *         building, null if the request is made by user.
+   */
+  public Building getDeliverTo() {
+    if (isUserRequest) {
+      return null;
+    } else {
+      return deliverTo;
     }
+  }
+  /**
+   * Tells if this request is made by user.
+   * @return true if the request is made by user, false if the request is issued by another building wanting the item.
+   */
+  public boolean isUserRequest() {
+    return isUserRequest;
+  }
 }
--- app/src/main/java/edu/duke/ece651/factorysim/World.java
+++ app/src/main/java/edu/duke/ece651/factorysim/World.java
@@ -3,19 +3,19 @@ package edu.duke.ece651.factorysim;
 import java.util.List;
 
 public class World {
-    public List<Building> buildings;
-    public List<Type> types;
-    public List<Recipe> recipes;
+  public List<Building> buildings;
+  public List<Type> types;
+  public List<Recipe> recipes;
 
-    public List<Building> getBuildings() {
-        return buildings;
-    }
+  public List<Building> getBuildings() {
+    return buildings;
+  }
 
-    public List<Type> getTypes() {
-        return types;
-    }
+  public List<Type> getTypes() {
+    return types;
+  }
 
-    public List<Recipe> getRecipes() {
-        return recipes;
-    }
+  public List<Recipe> getRecipes() {
+    return recipes;
+  }
 }
