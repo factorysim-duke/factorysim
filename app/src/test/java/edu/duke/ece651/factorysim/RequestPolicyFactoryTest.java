4954ffb8f5ae647d220d9232c9d1ef1d46404dda
Xingyu Shen <shen13515251251@gmail.com>
2025-03-15 00:21:08 -0400

    test: `RequestPolicyFactory`

--- app/src/test/java/edu/duke/ece651/factorysim/RequestPolicyFactoryTest.java
+++ app/src/test/java/edu/duke/ece651/factorysim/RequestPolicyFactoryTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class RequestPolicyFactoryTest {
   @Test
-  public void test_() {
-
+  public void test_createPolicy() {
+    RequestPolicy fifo = RequestPolicyFactory.createPolicy("fifo");
+    RequestPolicy ready = RequestPolicyFactory.createPolicy("ready");
+    // RequestPolicy sjf = RequestPolicyFactory.createPolicy("sjf");
+    assertEquals(fifo.getClass(), FifoRequestPolicy.class);
+    assertEquals(ready.getClass(), ReadyRequestPolicy.class);
+    assertThrows(IllegalArgumentException.class, () -> RequestPolicyFactory.createPolicy("unknown"));
   }
 
 }
