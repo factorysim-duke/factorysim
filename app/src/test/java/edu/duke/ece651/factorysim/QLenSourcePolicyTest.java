bb3cb4627c494891873de89a80786244807ad18b
sl846 <shiyu.liu2@duke.edu>
2025-03-18 02:34:53 -0400

    added more tests to QLenSourcePolicy, reached 100% coverage

--- app/src/test/java/edu/duke/ece651/factorysim/QLenSourcePolicyTest.java
+++ app/src/test/java/edu/duke/ece651/factorysim/QLenSourcePolicyTest.java
@@ -2,7 +2,6 @@ package edu.duke.ece651.factorysim;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,10 +10,11 @@ import org.junit.jupiter.api.Test;
 public class QLenSourcePolicyTest {
   private QLenSourcePolicy policy;
   private List<Building> sources;
-  MineBuilding woodMine;
-  MineBuilding metalMine1;
-  MineBuilding metalMine2;
-  MineBuilding metalMine3;
+  private MineBuilding woodMine;
+  private MineBuilding metalMine1;
+  private MineBuilding metalMine2;
+  private MineBuilding waterMine1;
+  private MineBuilding waterMine2;
   private FactoryBuilding doorFactory;
 
   @BeforeEach
@@ -25,19 +25,22 @@ public class QLenSourcePolicyTest {
     // set up mine buildings
     Simulation simulation = new TestUtils.MockSimulation();
     Recipe woodRecipe = TestUtils.makeTestRecipe("wood", 1, 1);
-    Recipe metalRecipe = TestUtils.makeTestRecipe("metal", 1, 1);
+    Recipe metalRecipe = TestUtils.makeTestRecipe("metal", 2, 1);
+    Recipe waterRecipe = TestUtils.makeTestRecipe("water", 4, 1);
     Recipe doorRecipe = TestUtils.makeTestRecipe("door", 12, 5);
     woodMine = new MineBuilding(woodRecipe, "W", simulation);
     metalMine1 = new MineBuilding(metalRecipe, "M1", simulation);
     metalMine2 = new MineBuilding(metalRecipe, "M2", simulation);
-    metalMine3 = new MineBuilding(metalRecipe, "M3", simulation);
+    waterMine1 = new MineBuilding(waterRecipe, "W1", simulation);
+    waterMine2 = new MineBuilding(waterRecipe, "W2", simulation);
 
     // set up sources
     sources = new ArrayList<>();
+    sources.add(woodMine);
     sources.add(metalMine1);
     sources.add(metalMine2);
-    sources.add(metalMine3);
-    sources.add(woodMine);
+    sources.add(waterMine1);
+    sources.add(waterMine2);
 
     // set up factory
     List<Recipe> recipes = new ArrayList<>();
@@ -60,5 +63,22 @@ public class QLenSourcePolicyTest {
     List<Building> availableSources = doorFactory.getAvailableSourcesForItem(wood);
     Building result = policy.selectSource(wood, availableSources);
     assertSame(woodMine, result);
+  }
+
+  @Test
+  public void test_equal_queue_length() {
+    Item metal = new Item("metal");
+    List<Building> availableSources = doorFactory.getAvailableSourcesForItem(metal);
+    Building result = policy.selectSource(metal, availableSources);
+    assertSame(metalMine1, result);
+  }
+
+  @Test
+  public void test_min_queue_length() {
+    Item water = new Item("water");
+    waterMine1.addRequest(new Request(0, water, waterMine1.getMiningRecipe(), metalMine1, doorFactory));
+    List<Building> availableSources = doorFactory.getAvailableSourcesForItem(water);
+    Building result = policy.selectSource(water, availableSources);
+    assertSame(waterMine2, result);
   }
 }
