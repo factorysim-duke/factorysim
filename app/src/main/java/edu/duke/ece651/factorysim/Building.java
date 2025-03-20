241d0095954ed2b16ff1f69a48ef4374c14d02fa
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-03-20 19:58:48 -0400

    `addRequest` now requests ingredients from sources
    Also indicates events to the simulation instance.
    Added `addPendingRequest` for test compatibility issue.

--- app/src/main/java/edu/duke/ece651/factorysim/Building.java
+++ app/src/main/java/edu/duke/ece651/factorysim/Building.java
@@ -22,8 +22,6 @@ public abstract class Building {
    * @param name          is the name of the building.
    * @param sources       is the list of buildings where this building can get
    *                      ingredients from.
-   * @param requestPolicy is the injected request policy instance for
-   *                      selecting a new request to process.
    * @throws IllegalArgumentException if the name is not valid.
    */
   protected Building(String name, List<Building> sources, Simulation simulation) {
@@ -136,12 +134,31 @@ public abstract class Building {
   }
 
   /**
-   * Add a new request to the request queue.
+   * Add a request as a pending request.
+   * NOTE: this method only adds the request, it doesn't request ingredients from sources like `addRequest`.
    *
-   * @param request The request to be added.
+   * @param request the request to be added.
+   */
+  public void addPendingRequest(Request request) {
+    pendingRequests.addFirst(request);
+  }
+
+  /**
+   * Add a new request to the request queue.<br/>
+   * The building MUST be able to process this request (because `Simulation` need to log sub-request details).
+   * NOTE: if you only want to add a request without requesting ingredients from sources, use `addPendingRequest`
+   *       instead.
+   *
+   * @param request the request to be added.
+   * @throws IllegalArgumentException when the building cannot process this request.
    */
   public void addRequest(Request request) {
-    pendingRequests.addFirst(request);
+    // Add request as a pending request
+    addPendingRequest(request);
+
+    // Send out requests for ingredients
+    List<Tuple<Item, Integer>> missingIngredients = findMissingIngredients2(request.getRecipe());
+    requestMissingIngredients2(missingIngredients);
   }
 
   /**
@@ -199,12 +216,31 @@ public abstract class Building {
       Recipe selectedRecipe = selectedRequest.getRecipe();
       if (hasAllIngredientsFor(selectedRecipe)) {
         consumeIngredientsFor(selectedRecipe);
-      } else {
-        HashMap<Item, Integer> missingIngredients = findMissingIngredients(selectedRecipe);
-        requestMissingIngredients(missingIngredients);
       }
     }
   }
+
+//  // A copy of the old `processRequest` that send out sub-requests to sources when request is selected
+//  protected void processRequest() {
+//    // if the building is processing a request, work on the current one
+//    if (isProcessing()) {
+//      boolean isRequestFinished = currentRequest.process();
+//      if (isRequestFinished) {
+//        finishCurrentRequest();
+//      }
+//    }
+//    // else, try to fetch the next one and work on it
+//    else if (pendingRequests.isEmpty() == false) {
+//      Request selectedRequest = requestPolicy.popRequest(this, pendingRequests);
+//      Recipe selectedRecipe = selectedRequest.getRecipe();
+//      if (hasAllIngredientsFor(selectedRecipe)) {
+//        consumeIngredientsFor(selectedRecipe);
+//      } else {
+//        HashMap<Item, Integer> missingIngredients = findMissingIngredients(selectedRecipe);
+//        requestMissingIngredients(missingIngredients);
+//      }
+//    }
+//  }
 
   /**
    * Checks if the things in storage are enough to produce the output of a recipe.
@@ -246,8 +282,11 @@ public abstract class Building {
     // if the request is not user request (has deliverTo destination building),
     // deliver the output item
     if (currentRequest.isUserRequest() == false) {
+      // deliver
       Building destinationBuilding = currentRequest.getDeliverTo();
       deliverTo(destinationBuilding, output, 1);
+      simulation.onIngredientDelivered(currentRequest.getItem(), destinationBuilding, this); // notify simulation
+
       // update our own storage
       takeFromStorage(output, 1);
     }
@@ -263,7 +302,56 @@ public abstract class Building {
    * recipe, considering current building storage.
    * Precondition: hasAllIngredientsFor(recipe) == false, thus there must be some
    * item whose number in storage is smaller than number needed in recipe
-   * 
+   *
+   * @param recipe is the recipe for reference.
+   * @return a list of missing ingredients.
+   */
+  public List<Tuple<Item, Integer>> findMissingIngredients2(Recipe recipe) {
+    List<Tuple<Item, Integer>> ingredients = new ArrayList<>();
+    for (Map.Entry<Item, Integer> entry : recipe.getIngredients().entrySet()) {
+      int need = entry.getValue();
+      int available = storage.getOrDefault(entry.getKey(), 0);
+      if (need > available) {
+        ingredients.add(new Tuple<>(entry.getKey(), need - available));
+      }
+    }
+    return ingredients;
+  }
+
+  /**
+   * Requests missing ingredients from sources.
+   * NOTE: this is where the source policy takes place.
+   *
+   * @param missingIngredients is a list of missing ingredients.
+   * @throws IllegalArgumentException if the sources of the building are not
+   *                                  enough to give missing items.
+   */
+  public void requestMissingIngredients2(List<Tuple<Item, Integer>> missingIngredients) {
+    for (Tuple<Item, Integer> entry : missingIngredients) {
+      Item item = entry.first();
+      int numNeeded = entry.second();
+      List<Building> availableSources = getAvailableSourcesForItem(item);
+      Building selectedSource = sourcePolicy.selectSource(item, availableSources);
+      if (selectedSource == null) {
+        throw new IllegalArgumentException("No source can produce the item " + item.getName());
+      }
+      Recipe recipeNeeded = simulation.getRecipeForItem(item);
+      // create sub-requests for numNeeded times for this item
+      for (int i = 0; i < numNeeded; i++) {
+        int orderNum = simulation.getOrderNum(); // this function automatically proceed the next order num by 1
+        Request subRequest = new Request(orderNum, item, recipeNeeded, selectedSource, this);
+        simulation.onIngredientAssigned(item, selectedSource, this);
+        selectedSource.addRequest(subRequest);
+      }
+    }
+  }
+
+  /**
+   * Finds the missing ingredients item and corresponding quantity for a given
+   * recipe, considering current building storage.
+   * Precondition: hasAllIngredientsFor(recipe) == false, thus there must be some
+   * item whose number in storage is smaller than number needed in recipe
+   *
    * @return recipe is the recipe for reference.
    * @return the hashmap for missing ingredients.
    */
@@ -305,6 +393,9 @@ public abstract class Building {
         int orderNum = simulation.getOrderNum(); // this function automatically proceed the next order num by 1
         Request subRequest = new Request(orderNum, item, recipeNeeded, selectedSource, this);
         selectedSource.addRequest(subRequest);
+
+        // notify simulation about ingredient assignment
+        simulation.onIngredientAssigned(item, selectedSource, this);
       }
     }
   }
@@ -390,12 +481,5 @@ public abstract class Building {
    * @param item is the item to be checked.
    * @return true if this building can produce this item, false otherwise.
    */
-  /**
-   * Checks if this building can produce a given item.
-   *
-   * @param item is the item to be checked.
-   * @return true if this building can produce this item, false otherwise.
-   */
   public abstract boolean canProduce(Item item);
-
 }
