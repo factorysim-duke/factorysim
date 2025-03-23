4784cc5670ea353b2fa29ea1dd5ee6b951f83250
Xingyu Shen <shen13515251251@gmail.com>
2025-03-23 14:55:08 -0400

    create: test for RecursiveLatSourcePolicy.java

--- app/src/main/java/edu/duke/ece651/factorysim/RecursiveLatSourcePolicy.java
+++ app/src/main/java/edu/duke/ece651/factorysim/RecursiveLatSourcePolicy.java
@@ -1,30 +1,64 @@
 package edu.duke.ece651.factorysim;
 
-import java.util.List;
+import java.util.*;
 import java.util.function.BiConsumer;
 
 /**
  * Implements recursive latency (recursivelat) source policy.
  */
 public class RecursiveLatSourcePolicy extends SourcePolicy {
+  private static class Usage {
+    private Map<String, Map<List<BuildingId>, Integer>> storageUsed = new HashMap<>();
+  }
+
+  private static class BuildingId {
+    private Building building;
+    private String uniqueId;
+
+    public BuildingId(Building building, String uniqueId) {
+      this.building = building;
+      this.uniqueId = uniqueId;
+    }
+  }
+
+  private int estimate(Request request, Building factory, Usage usage, List<BuildingId> path) {
+    return 0; // TODO: Implement this
+  }
+
+  private int calculateTotalEstimate(Building source) {
+    Usage usage = new Usage();
+    int total = 0;
+    // TODO: Implement calculation
+    return total;
+  }
+
   /**
    * Selects a source to produce item according to the policy.
    * 
-   * @param item    is the requested item.
-   * @param sources is the list of buildings that can produce the item.
+   * @param item          is the requested item.
+   * @param sources       is the list of buildings that can produce the item.
    * @param onReportScore is the callback function to report score.
    * @return the source building according to the policy.
    */
   @Override
   public Building selectSource(Item item,
-                               List<Building> sources,
-                               BiConsumer<Building, Integer> onReportScore) {
+      List<Building> sources,
+      BiConsumer<Building, Integer> onReportScore) {
     if (sources.isEmpty()) {
       return null;
     }
     Building bestSource = sources.get(0);
-    // build here
-    // TODO: please make sure to call `onReportScore.accept(source, score)` here so verbosity 2 can log scores
+    int minScore = calculateTotalEstimate(bestSource);
+    onReportScore.accept(bestSource, minScore);
+    for (int i = 1; i < sources.size(); i++) {
+      Building source = sources.get(i);
+      int score = calculateTotalEstimate(source);
+      onReportScore.accept(source, score);
+      if (score < minScore) {
+        minScore = score;
+        bestSource = source;
+      }
+    }
     return bestSource;
   }
 
