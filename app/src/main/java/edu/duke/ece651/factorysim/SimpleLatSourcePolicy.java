104b23b52cdd70eddd7dcd67c44414825a077cd5
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-03-23 03:55:21 -0400

    Adds a lambda function to report source score

--- app/src/main/java/edu/duke/ece651/factorysim/SimpleLatSourcePolicy.java
+++ app/src/main/java/edu/duke/ece651/factorysim/SimpleLatSourcePolicy.java
@@ -1,6 +1,7 @@
 package edu.duke.ece651.factorysim;
 
 import java.util.List;
+import java.util.function.BiConsumer;
 
 /**
  * Implements simple latency (simplelat) source policy.
@@ -11,10 +12,13 @@ public class SimpleLatSourcePolicy extends SourcePolicy {
    * 
    * @param item    is the requested item.
    * @param sources is the list of buildings that can produce the item.
+   * @param onReportScore is the callback function to report score.
    * @return the source building according to the policy.
    */
   @Override
-  public Building selectSource(Item item, List<Building> sources) {
+  public Building selectSource(Item item,
+                               List<Building> sources,
+                               BiConsumer<Building, Integer> onReportScore) {
     if (sources.isEmpty()) {
       return null;
     }
@@ -22,6 +26,7 @@ public class SimpleLatSourcePolicy extends SourcePolicy {
     int minRemainingLatencies = bestSource.sumRemainingLatencies();
     for (Building source : sources) {
       int newNum = source.sumRemainingLatencies();
+      onReportScore.accept(source, newNum); // Report source score
       if (newNum < minRemainingLatencies) {
         minRemainingLatencies = newNum;
         bestSource = source;
--- app/src/main/java/edu/duke/ece651/factorysim/SourcePolicy.java
+++ app/src/main/java/edu/duke/ece651/factorysim/SourcePolicy.java
@@ -1,6 +1,7 @@
 package edu.duke.ece651.factorysim;
 
 import java.util.List;
+import java.util.function.BiConsumer;
 
 /**
  * Defines a policy for selecting a source in the simulation.
@@ -8,12 +9,27 @@ import java.util.List;
 public abstract class SourcePolicy implements Policy {
   /**
    * Selects a source to produce item according to the policy.
+   * Report calculated score through `onReportScore`.
    * 
+   * @param item    is the requested item.
+   * @param sources is the list of buildings that can produce the item.
+   * @param onReportScore is the callback function to report score.
+   * @return the source building according to the policy.
+   */
+  public abstract Building selectSource(Item item,
+                                        List<Building> sources,
+                                        BiConsumer<Building, Integer> onReportScore);
+
+  /**
+   * Selects a source to produce item according to the policy.
+   *
    * @param item    is the requested item.
    * @param sources is the list of buildings that can produce the item.
    * @return the source building according to the policy.
    */
-  public abstract Building selectSource(Item item, List<Building> sources);
+  public Building selectSource(Item item, List<Building> sources) {
+    return selectSource(item, sources, (b, i) -> { });
+  }
 
   /**
    * Gets the policy type's name.
