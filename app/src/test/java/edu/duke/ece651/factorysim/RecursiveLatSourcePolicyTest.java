9881b1311d1ddc05c6a26409faa67343c5aef68d
Xingyu Shen <shen13515251251@gmail.com>
2025-03-23 15:04:08 -0400

    add some basic tests for RecursiveLatsourcePolicy

--- app/src/test/java/edu/duke/ece651/factorysim/RecursiveLatSourcePolicyTest.java
+++ app/src/test/java/edu/duke/ece651/factorysim/RecursiveLatSourcePolicyTest.java
@@ -1,13 +1,73 @@
 package edu.duke.ece651.factorysim;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RecursiveLatSourcePolicyTest {
-  @Test
-  public void test_() {
+  private RecursiveLatSourcePolicy policy;
+  private List<Building> sources;
+  private MineBuilding woodMine;
+  private MineBuilding metalMine1;
+  private MineBuilding metalMine2;
+  private MineBuilding waterMine1;
+  private MineBuilding waterMine2;
+  private FactoryBuilding doorFactory;
 
+  @BeforeEach
+  public void setUp() {
+    // set up policy
+    policy = new RecursiveLatSourcePolicy();
+
+    // set up mine buildings
+    Simulation simulation = new TestUtils.MockSimulation();
+    Recipe woodRecipe = TestUtils.makeTestRecipe("wood", 1, 1);
+    Recipe metalRecipe = TestUtils.makeTestRecipe("metal", 2, 1);
+    Recipe waterRecipe = TestUtils.makeTestRecipe("water", 4, 1);
+    Recipe doorRecipe = TestUtils.makeTestRecipe("door", 12, 5);
+    woodMine = new MineBuilding(woodRecipe, "W", simulation);
+    metalMine1 = new MineBuilding(metalRecipe, "M1", simulation);
+    metalMine2 = new MineBuilding(metalRecipe, "M2", simulation);
+    waterMine1 = new MineBuilding(waterRecipe, "W1", simulation);
+    waterMine2 = new MineBuilding(waterRecipe, "W2", simulation);
+
+    // set up sources
+    sources = new ArrayList<>();
+    sources.add(woodMine);
+    sources.add(metalMine1);
+    sources.add(metalMine2);
+    sources.add(waterMine1);
+    sources.add(waterMine2);
+
+    // set up factory
+    List<Recipe> recipes = new ArrayList<>();
+    recipes.add(doorRecipe);
+    Type doorFactoryType = new Type("door", recipes);
+    doorFactory = new FactoryBuilding(doorFactoryType, "D", sources, simulation);
+  }
+
+  @Test
+  public void test_select_from_empty_sources() {
+    Item invalid_item = new Item("invalid");
+    List<Building> availableSources = doorFactory.getAvailableSourcesForItem(invalid_item);
+    Building result = policy.selectSource(invalid_item, availableSources);
+    assertNull(result);
+  }
+
+  @Test
+  public void test_select_single_possibility() {
+    Item wood = new Item("wood");
+    List<Building> availableSources = doorFactory.getAvailableSourcesForItem(wood);
+    Building result = policy.selectSource(wood, availableSources);
+    assertSame(woodMine, result);
+  }
+
+  @Test
+  public void test_get_name() {
+    assertEquals("recursivelat", policy.getName());
   }
 
 }
