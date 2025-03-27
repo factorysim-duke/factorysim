8f35e27fedd09fc701a2dc95e7d7a0ad2b19ba1b
sl846 <shiyu.liu2@duke.edu>
2025-03-26 21:49:49 -0400

    added javadoc for StreamLogger.java

--- app/src/main/java/edu/duke/ece651/factorysim/StreamLogger.java
+++ app/src/main/java/edu/duke/ece651/factorysim/StreamLogger.java
@@ -8,10 +8,20 @@ import java.io.*;
 public class StreamLogger implements Logger {
   private final PrintWriter writer;
 
+  /**
+   * Constructs a stream logger.
+   * 
+   * @param stream is the output stream of the logger.
+   */
   public StreamLogger(OutputStream stream) {
     this.writer = new PrintWriter(stream, true);
   }
 
+  /**
+   * Logs to the stream.
+   * 
+   * @param s is the string to be logged to stream.
+   */
   @Override
   public void log(String s) {
     writer.println(s);
