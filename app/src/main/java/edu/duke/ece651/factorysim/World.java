430cd338e99c2ed2cff595ca77448778ece22375
sl846 <shiyu.liu2@duke.edu>
2025-03-26 21:55:51 -0400

    added javadoc for World.java

--- app/src/main/java/edu/duke/ece651/factorysim/World.java
+++ app/src/main/java/edu/duke/ece651/factorysim/World.java
@@ -2,31 +2,64 @@ package edu.duke.ece651.factorysim;
 
 import java.util.List;
 
+/**
+ * Represents a world in the simulation which holds all the information.
+ */
 public class World {
   private List<Building> buildings;
   private List<Type> types;
   private List<Recipe> recipes;
 
+  /**
+   * Gets all the buildings of the world.
+   * 
+   * @return the list of buildings in the world.
+   */
   public List<Building> getBuildings() {
     return buildings;
   }
 
+  /**
+   * Gets all the factory types in the world.
+   * 
+   * @return the list of factory types in the world.
+   */
   public List<Type> getTypes() {
     return types;
   }
 
+  /**
+   * Gets all the recipes in the world.
+   * 
+   * @return the list of recipes in the world.
+   */
   public List<Recipe> getRecipes() {
     return recipes;
   }
 
+  /**
+   * Sets the buildings in the world.
+   * 
+   * @param buildings is the list of buildings to set.
+   */
   public void setBuildings(List<Building> buildings) {
     this.buildings = buildings;
   }
 
+  /**
+   * Sets the factory types in the world.
+   * 
+   * @param types is the list of factory types to set.
+   */
   public void setTypes(List<Type> types) {
     this.types = types;
   }
 
+  /**
+   * Sets the recipes in the world.
+   * 
+   * @param recipes is the list of recipes to set.
+   */
   public void setRecipes(List<Recipe> recipes) {
     this.recipes = recipes;
   }
@@ -76,6 +109,7 @@ public class World {
 
   /**
    * Gets a Type by its name.
+   * 
    * @param name The name of the type.
    * @return The Type object, or null if not found.
    */
@@ -90,6 +124,7 @@ public class World {
 
   /**
    * Gets a Recipe by its name (output item name).
+   * 
    * @param name The name of the recipe's output item.
    * @return The Recipe object, or null if not found.
    */
@@ -101,6 +136,4 @@ public class World {
     }
     return null;
   }
-
-
 }
