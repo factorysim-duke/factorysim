92af91bb217a523914cf20266c176465ced8677b
sl846 <shiyu.liu2@duke.edu>
2025-04-16 19:56:38 -0400

    100% test coverage for refactored Recipe.java

--- app/src/test/java/edu/duke/ece651/factorysim/RecipeTest.java
+++ app/src/test/java/edu/duke/ece651/factorysim/RecipeTest.java
@@ -15,15 +15,18 @@ public class RecipeTest {
     Item handle = new Item("handle");
     Item hinge = new Item("hinge");
     Item door = new Item("door");
+    Item dust = new Item("dust");
 
     // build the ingredients hashmap for constructor
     HashMap<Item, Integer> ingredients = new HashMap<>();
     ingredients.put(wood, 1);
     ingredients.put(handle, 2);
     ingredients.put(hinge, 3);
+    HashMap<Item, Integer> waste = new HashMap<>();
+    waste.put(dust, 10);
 
     // constructs the recipe
-    Recipe doorRecipe = new Recipe(door, ingredients, 12);
+    Recipe doorRecipe = new Recipe(door, ingredients, 12, waste);
 
     // test the ingredients
     HashMap<Item, Integer> doorIngredients = doorRecipe.getIngredients();
@@ -38,24 +41,32 @@ public class RecipeTest {
     // test output and latency
     assertSame(door, doorRecipe.getOutput());
     assertEquals(12, doorRecipe.getLatency());
+
+    // test waste
+    HashMap<Item, Integer> doorWaste = doorRecipe.getWasteByProducts();
+    assertTrue(doorWaste.containsKey(dust));
+    assertEquals(10, doorWaste.get(dust));
   }
 
   @Test
-  public void test_toJSON() {
+  public void test_toJSON_with_waste() {
     Item wood = new Item("wood");
     Item metal = new Item("metal");
     Item hinge = new Item("hinge");
+    Item woodDust = new Item("woodDust");
     HashMap<Item, Integer> ingredients = new HashMap<>();
     ingredients.put(wood, 1);
     ingredients.put(metal, 2);
-
-    Recipe recipe=new Recipe(hinge,  ingredients,5);
+    HashMap<Item, Integer> waste = new HashMap<>();
+    waste.put(woodDust, 1);
+    Recipe recipe = new Recipe(hinge, ingredients, 5, waste);
     JsonObject json = recipe.toJson();
-    assertEquals("hinge",json.get("output").getAsString());
-
+    assertEquals("hinge", json.get("output").getAsString());
     JsonObject ingredientsJson = json.getAsJsonObject("ingredients");
-    assertEquals(2,ingredientsJson.get("metal").getAsInt());
-    assertEquals(1,ingredientsJson.get("wood").getAsInt());
-    assertEquals(5,json.get("latency").getAsInt());
+    assertEquals(2, ingredientsJson.get("metal").getAsInt());
+    assertEquals(1, ingredientsJson.get("wood").getAsInt());
+    assertEquals(5, json.get("latency").getAsInt());
+    JsonObject wasteJson = json.getAsJsonObject("waste");
+    assertEquals(1, wasteJson.get("woodDust").getAsInt());
   }
 }
