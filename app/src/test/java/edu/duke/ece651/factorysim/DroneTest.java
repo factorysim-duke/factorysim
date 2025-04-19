236067534fd84ddeeae5ad80b8624318ad7cb70c
sl846 <shiyu.liu2@duke.edu>
2025-04-19 07:28:54 -0400

    full test coverage for Drone.java

--- app/src/test/java/edu/duke/ece651/factorysim/DroneTest.java
+++ app/src/test/java/edu/duke/ece651/factorysim/DroneTest.java
@@ -1,13 +1,21 @@
 package edu.duke.ece651.factorysim;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DroneTest {
   @Test
-  public void test_() {
-
+  void test_drone_methods() {
+    Drone drone = new Drone();
+    assertFalse(drone.isInUse());
+    drone.setInUse(true);
+    assertTrue(drone.isInUse());
+    drone.setInUse(false);
+    assertFalse(drone.isInUse());
+    assertEquals(5, Drone.getSpeed());
+    drone.setInUse(true);
+    assertEquals(true, drone.toJson().get("inUse").getAsBoolean());
+    drone.setInUse(false);
+    assertEquals(false, drone.toJson().get("inUse").getAsBoolean());
   }
-
 }
